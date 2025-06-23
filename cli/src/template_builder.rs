b527abfdbb5e8456ea9c6c50e4fe4bb1329f3a72
Bryce Berger <bryce.z.berger@gmail.com>
2025-06-22 21:19:08 -0400

    log: add `__width` variable containing printable width
    takes into account both the terminal size and the width taken up by
    the graph

--- cli/src/template_builder.rs
+++ cli/src/template_builder.rs
@@ -2021,6 +2021,7 @@ pub fn build_expression<'a, L: TemplateLanguage<'a> + ?Sized>(
 pub fn build<'a, C, L>(
     language: &L,
     diagnostics: &mut TemplateDiagnostics,
+    local_variables: HashMap<&str, &dyn Fn() -> L::Property>,
     node: &ExpressionNode,
 ) -> TemplateParseResult<TemplateRenderer<'a, C>>
 where
@@ -2030,7 +2031,7 @@ where
 {
     let self_placeholder = PropertyPlaceholder::new();
     let build_ctx = BuildContext {
-        local_variables: HashMap::new(),
+        local_variables,
         self_variable: &|| self_placeholder.clone().into_dyn_wrapped(),
     };
     let template = expect_template_expression(language, diagnostics, &build_ctx, node)?;
@@ -2042,6 +2043,7 @@ pub fn parse<'a, C, L>(
     language: &L,
     diagnostics: &mut TemplateDiagnostics,
     template_text: &str,
+    local_variables: HashMap<&str, &dyn Fn() -> L::Property>,
     aliases_map: &TemplateAliasesMap,
 ) -> TemplateParseResult<TemplateRenderer<'a, C>>
 where
@@ -2050,7 +2052,8 @@ where
     L::Property: WrapTemplateProperty<'a, C>,
 {
     let node = template_parser::parse(template_text, aliases_map)?;
-    build(language, diagnostics, &node).map_err(|err| err.extend_alias_candidates(aliases_map))
+    build(language, diagnostics, local_variables, &node)
+        .map_err(|err| err.extend_alias_candidates(aliases_map))
 }
 
 pub fn expect_boolean_expression<'a, L: TemplateLanguage<'a> + ?Sized>(
@@ -2242,6 +2245,7 @@ mod tests {
                 &self.language,
                 &mut TemplateDiagnostics::new(),
                 template,
+                Default::default(),
                 &self.aliases_map,
             )
         }
